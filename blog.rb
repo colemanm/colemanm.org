9eea11274a6940bfc6de1c32bb70f4b129690289
Coleman McCormick <coleman@mcco.me>
2018-11-26 00:19:47 -0500

    Update generate script

--- blog.rb
+++ blog.rb
@@ -69,6 +69,41 @@ class Blog < Thor
     puts "Link '#{date}-#{slug}.md' created."
   end
 
+  desc 'book', 'Create a new book in library.'
+  method_option :title, aliases: '-t', desc: 'Book title', default: 'New book'
+  method_option :author, aliases: '-a', desc: 'Book author last name'
+  def book
+    title       = options[:title]
+    author_last = options[:author]
+    slug        = author_last.downcase.strip.tr(' ', '-').gsub(/[^\w-]/, '') + "-" + title.downcase.strip.tr(' ', '-').gsub(/[^\w-]/, '')
+    file        = File.join('./_books/', "#{slug}.md")
+    open(file, 'w') do |link|
+      link.puts <<~BOOK
+        ---
+        layout: book
+        title: "#{title}"
+        title_full: "#{title}"
+        author: 
+        author_last: #{author_last}
+        slug: #{slug}
+        type: 
+        genres:
+        - 
+        isbn: 
+        rating: 
+        pages: 
+        format: 
+        publish_year: 
+        date_started: 
+        date_completed: 
+        goodreads_id: 
+        ---
+      BOOK
+    end
+
+    puts "Book '#{slug}.md' created."
+  end
+
   no_tasks do
     def generate_date(postdate)
       (postdate ? Time.parse(postdate) : Time.now).strftime('%F')
