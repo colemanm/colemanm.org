b7883aab22e1b234ccffce6a6f0f0dbf0231cbf2
Coleman McCormick <coleman@mcco.me>
2025-02-14 15:58:06 -0500

    Fonts and design

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,22 +12,65 @@ module.exports = {
           DEFAULT: {
             css: {
               a: {
-                color: '#517AA3',
+                color: '#446688',
+                textDecoration: 'none',
+                borderBottom: '1px solid #bcd',
                 '&:hover': {
-                  color: '#3d5d7d',
+                  color: '#2d3939',
+                  borderBottom: '1px solid #dd9922',
                 },
               },
-              h1: {
+              'span.favorite-star': {
+                fontSize: '14px', 
+                color: '#ffb800',
+                verticalAlign: 'middle',
+                marginLeft: '0.25em',
+              },
+              'a.font-bold': {
+                fontWeight: '700',
+              },
+              strong: {
+                fontWeight: '700',
+                color: 'inherit',
+              },
+              'h1': {
+                color: '#517AA3',
+                marginTop: '2.5rem',
+                marginBottom: '2.5rem',
+                '&:first-child': {
+                  marginTop: '2.5rem',
+                }
+              },
+              'h2': {
                 color: '#517AA3',
               },
-              h2: {
-                color: '#517AA3',
+              'code::before': {
+                content: 'none !important',
+              },
+              'code::after': {
+                content: 'none !important',
+              },
+              'tbody td:last-child': {
+                textAlign: 'right',
+              },
+              'thead th:last-child': {
+                textAlign: 'right',
+              },
+              code: {
+                backgroundColor: '#f2ede8',
+                padding: '0.25rem',
+              },
+              'pre code::before': {
+                content: 'none !important',
+              },
+              'pre code::after': {
+                content: 'none !important',
               },
             },
           },
         },
         fontFamily: {
-          sans: ['Inter', 'sans-serif'],
+          sans: ['Instrument Sans', 'sans-serif'],
         },
       },
     },
