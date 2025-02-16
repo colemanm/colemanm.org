1a45ce5753719d7683a558124fc8cc69a5a6105c
Coleman McCormick <coleman@mcco.me>
2025-02-15 22:54:35 -0500

    captions, book layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,20 @@ module.exports = {
                   borderBottom: '1px solid #dd9922',
                 },
               },
+              'blockquote': {
+                fontStyle: 'normal',
+                quotes: 'none',
+                backgroundColor: '#f8fafc',
+                borderLeft: '3px solid #e2e8f0',
+                padding: '0.5rem 1.0rem',
+                margin: '1.5rem 0',
+              },
+              'blockquote p:first-of-type::before': {
+                content: 'none',
+              },
+              'blockquote p:last-of-type::after': {
+                content: 'none',
+              },
               'span.favorite-star': {
                 fontSize: '14px', 
                 color: '#ffb800',
@@ -33,12 +47,37 @@ module.exports = {
                 fontWeight: '700',
                 color: 'inherit',
               },
+              'ul, ol': {
+                marginTop: '0.25rem',
+                marginBottom: '0.25rem',
+                '& ul, & ol': {
+                  marginTop: '0.25rem',
+                  marginBottom: '0.25rem',
+                }
+              },
+              'li': {
+                marginTop: '0.25rem',
+                marginBottom: '0.25rem',
+              },
               'h1': {
                 color: '#517AA3',
                 marginTop: '2.5rem',
                 marginBottom: '2.5rem',
                 '&:first-child': {
                   marginTop: '2.5rem',
+                }
+              },
+              '.caption': {
+                display: 'block',
+                textAlign: 'center',
+                color: '#666666',
+                fontSize: '0.875rem',
+              },
+              'figure': {
+                margin: '1.5rem 0',
+                '& img': {
+                  marginBottom: '0.5rem',
+                  marginTop: '0',
                 }
               },
               'h2': {
@@ -55,6 +94,10 @@ module.exports = {
               },
               'thead th:last-child': {
                 textAlign: 'right',
+              },
+              '.book-date-completed': {
+                width: '6.5rem',
+                whiteSpace: 'nowrap',
               },
               code: {
                 backgroundColor: '#f2ede8',
